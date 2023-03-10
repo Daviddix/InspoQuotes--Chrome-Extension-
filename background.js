8ad1c765c84e8326860305a9137238af42e5e7ae
Emmanuel Nsikan-David <en56434@gmail.com>
2023-03-10 22:35:57 +0100

    working fetch and notifications

--- background.js
+++ background.js
@@ -1,24 +1,42 @@
-const notificationOptions = {
-    title:"Herert Mecurley",
-    message: "Today is a great day",
-    type:"basic",
-    iconUrl: "/assets/images/test.jpeg",
-    priority: 1
-}
-
 chrome.runtime.onMessage.addListener((message)=>{
     if (message.quotesSettings) {
-        console.log(message.quotesSettings.interval)
-         chrome.alarms.create(
+    const categories = message.quotesSettings.categories
+    let i = 0
+
+    const url = `https://api.api-ninjas.com/v1/quotes?category=${categories[i]}`
+    const headers = {"X-Api-Key" : "gJY8u11Am0dTYp9oq0G5Lg==ILAKhRRxmPrPKKVs", "Content-Type" : "application/json"}
+
+        chrome.alarms.create(
             "new inspoQuotes", 
             {periodInMinutes : .2})
 
     chrome.alarms.onAlarm.addListener(()=>{
+        fetch(url, {headers})
+        .then((raw)=> raw.json())
+        .then((data)=>{
+            if (data) {
+                if (i == categories.length - 1) {
+                    i = 0
+                }
         chrome.notifications.create(
-            `inspoQuote ${Date.now()}`,
-            notificationOptions
-       )
-    })    
+        `inspoQuote ${Date.now()}`,
+        {
+            title:`${data[0].author} - ${data[0].category}`,
+            message: data[0].quote,
+            type:"basic",
+            iconUrl: "/assets/images/test.jpeg",
+            priority: 1
+        })
+        i++
+        }else{
+            throw new Error
+        }
+        })
+        .catch((err)=>{
+            alert(err)
+        })       
+    })
+      
     }else{
         chrome.alarms.clearAll()
     }
