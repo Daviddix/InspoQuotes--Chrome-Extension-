8a40de3f0d1d69757a21b1b4c1a98193cea966f6
Emmanuel Nsikan-David <en56434@gmail.com>
2023-03-13 11:27:38 +0100

    fixed multiple calling

--- background.js
+++ background.js
@@ -4,21 +4,46 @@ chrome.runtime.onMessage.addListener((message)=>{
     let i = 0
     let url
     let shortQuotes = []
-
-    
     const headers = {"X-Api-Key" : "gJY8u11Am0dTYp9oq0G5Lg==ILAKhRRxmPrPKKVs", "Content-Type" : "application/json"}
 
-        chrome.alarms.create(
-            "new inspoQuotes", 
-            {periodInMinutes : .2})
+    chrome.alarms.create("new inspoQuotes", {periodInMinutes : .2})
 
-    chrome.alarms.onAlarm.addListener(()=>{
-        if (categories.length == 1 && categories[0] == "Random") {
-            url = `https://api.api-ninjas.com/v1/quotes?category=&limit=10`
+    if (categories.includes("Random") === true) {
+       chrome.alarms.onAlarm.addListener(()=>{
+        url = `https://api.api-ninjas.com/v1/quotes?category=&limit=10`
+    
+        fetch(url, {headers})
+        .then((raw)=> raw.json())
+        .then((data)=>{
+            if (data) {
+        data.forEach((quote)=>{
+            if (quote.quote.length <= 130) {
+                shortQuotes.push(quote)
+            }
+        })
+        let num = Math.floor(Math.random() * shortQuotes.length - 1)
+        chrome.notifications.create(
+        `inspoQuote ${Date.now()}`,
+        {
+            title:`${shortQuotes[num].author} - ${shortQuotes[num].category}`,
+            message: shortQuotes[num].quote,
+            type:"basic",
+            iconUrl: "/assets/images/test.jpeg",
+            priority: 1
+        })
         }else{
-            url = `https://api.api-ninjas.com/v1/quotes?category=${categories[i]}&limit=10`
+            throw new Error
         }
-        
+        })
+        .catch((err)=>{
+            chrome.alarms.clearAll()
+            console.log("an error occured");
+        })       
+    })
+    }else if(categories.includes("Random") === false){
+        chrome.alarms.onAlarm.addListener(()=>{
+        url = `https://api.api-ninjas.com/v1/quotes?category=${categories[i]}&limit=10`    
+    
         fetch(url, {headers})
         .then((raw)=> raw.json())
         .then((data)=>{
@@ -41,8 +66,8 @@ chrome.runtime.onMessage.addListener((message)=>{
             iconUrl: "/assets/images/test.jpeg",
             priority: 1
         })
+        
         i++
-        console.log(url)
         }else{
             throw new Error
         }
@@ -53,9 +78,11 @@ chrome.runtime.onMessage.addListener((message)=>{
         })       
     })
       
+    }
+      
     }else{
         chrome.alarms.clearAll()
+        chrome.runtime.sendMessage("turn-app-off");
     }
 })
-
 
